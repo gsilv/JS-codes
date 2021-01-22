4272f51757f741141bf4f934a2516bcf8e0efb73
gsilv <66849275+gsilv@users.noreply.github.com>
2021-01-22 15:58:29 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -2,23 +2,47 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) => { 
-    res.send({ nome: 'Notebook', preco: 123.45})
+// app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: true  }))
+
+app.get('/produtos', (req, res) => { 
+    // console.log({ produtos: bancoDeDados.getProdutos() })
+    res.send(bancoDeDados.getProdutos())
 })
 
 app.get('/produtos/:id', (req, res) => {
-    res.send(bancoDeDados.getProdutos(req.params.id))
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.get('/produtos', (req, res) => {
+app.post('/produtos', (req, res) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto)
 })
+
+app.put('/produtos/:id', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res) => {
+    const produto = bancoDeDados.deleteProduto(req.params.id
+        // nome: req.body.nome,
+        // preco: req.body.preco
+    )
+    res.send(produto)
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}`)
